375109f31dae7a220a855b0a1341a33be6dd8019
Johny-Hinchliffe <82289369+Johny-Hinchliffe@users.noreply.github.com>
2022-03-01 08:35:12 +0000

    finished edit and create requests, moving onto delete now

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,27 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { Router, Route } from 'react-router-dom'
 import StreamList from './streams/StreamList'
 import StreamEdit from './streams/StreamEdit'
 import StreamDelete from './streams/StreamDelete'
 import StreamShow from './streams/StreamShow'
 import StreamCreate from './streams/StreamCreate'
 import Header from './Header'
-//BrowserRouter - Most difficult to deploy but not easier
+import history from '../history'
+
+//Router - Most difficult to deploy but not easier
 // HashRouter - # after
 // MemoryRouter - no change of URL /
 
 const App = () => {
 	return (
 		<div className="ui container">
-			<Router>
+			<Router history={history}>
 				<Header />
-				<Routes>
-					<Route path="/" element={<StreamList />} />
-					<Route path="/streams/create" element={<StreamCreate />} />
-					<Route path="/streams/edit" element={<StreamEdit />} />
-					<Route path="/streams/delete" element={<StreamDelete />} />
-					<Route path="/streams/show" element={<StreamShow />} />
-				</Routes>
+				<Route path="/" exact component={StreamList} />
+				<Route path="/streams/new" exact component={StreamCreate} />
+				<Route path="/streams/edit/:id" exact component={StreamEdit} />
+				<Route path="/streams/delete" exact component={StreamDelete} />
+				<Route path="/streams/show" exact component={StreamShow} />
 			</Router>
 		</div>
 	)
--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import _ from 'lodash'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { fetchStream, editStream } from '../../actions'
+import StreamForm from './StreamForm'
 
-const StreamEdit = () => {
-  return (
-    <div>StreamEdit</div>
-  )
+const StreamEdit = (props) => {
+  const {id} = props.match.params
+	useEffect(() => {
+		props.fetchStream(id)
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
+
+	const onSubmit = (formValues) => {
+		props.editStream(id,formValues)
+	}
+	if (!props.stream) {
+		return <div className="ui loading">Loading...</div>
+	}
+	return (
+		<div className="">
+			<h3>Edit your Stream</h3>
+			<StreamForm 
+        initialValues={_.pick(props.stream, 'title', 'description')}
+        onSubmit={onSubmit} 
+        />
+		</div>
+	)
+}
+const mapStateToProps = (state, ownProps) => {
+	return { stream: state.streams[ownProps.match.params.id] }
 }
 
-export default StreamEdit
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit)
